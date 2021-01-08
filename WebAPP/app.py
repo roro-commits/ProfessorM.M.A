14786381eaf739e2a77e3bbe1a2f83349511387c
roro-commits <olarotimi@protonmail.com>
2021-01-08 05:28:55 +0000

    Web app with back end

---
+++ WebAPP/app.py
@@ -0,0 +1,143 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder as encoder, MinMaxScaler
+
+
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+# class ufc(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     pass
+#
+# class Form(FlaskForm):
+#     name = SelectField('NAME', choices=[])
+#
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    print("ccheking2", flush=True)
+    return render_template('index.html')
+
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    '''
+    Renders result in HTML
+    '''
+    if request.method == 'POST':
+        print("test1", flush=True)
+
+        if 'form' in request.form:
+
+            # after receving data
+            print("recived data", flush=True)
+            data = request.form['form']
+            # dataset = [x for x in data]
+            dataset = lambda x: list(x.split(","))
+            dataset = dataset(data)
+            print("Datasets", dataset, flush=True)
+            print("Datasets", dataset[0], flush=True)
+            # idex not to conver = 0,4,14,18
+            # Convert for adding to data frame
+            for x in range(0, len(dataset)):
+                if (x == 0) or (x == 4) or (x == 14) or (x == 18):
+                    pass
+                else:
+                    dataset[x] = float(dataset[x])
+                    print(dataset[x])
+            # apppend one for extra column for dataframe to be dropped later 
+            dataset.append(1)
+            print("Datasets", dataset, flush=True)
+            print("Datasets", type(dataset[1]), flush=True)
+
+            # df = pd.DataFrame(dataset)
+            # # df.replace(stance)
+            # df.replace(regex=['Orthodox'], value=1, inplace=True)
+            # df.replace(regex=['Southpaw'], value=3, inplace=True)
+            # df.replace(regex=['Switch'], value=4, inplace=True)
+            # df.replace(regex=['Open Stance'], value=1, inplace=True)
+            # df.replace(regex=['Sideways'], value=1, inplace=True)
+            # print("Datasets", df, flush=True)
+            # print(df, flush=True)
+            # final_features = df[0:].values.tolist()
+            # result = np.array(final_features)
+            # result = result.T
+            # print("result", result.T, flush=True)
+            # result = result.astype(float)
+            # result = result[0]
+            # print('final feature *** ', result, flush=True)
+
+            UFC_data = pd.read_csv('clean_dataset.csv')
+            UFC_data = UFC_data.dropna()
+            length_dataframe = len(UFC_data)
+            UFC_data.loc[length_dataframe] = dataset
+            print(UFC_data.iloc[-1])
+            UFC_data['STANCE'] = encoder().fit_transform(UFC_data['STANCE'])
+            UFC_data['STANCE1'] = encoder().fit_transform(UFC_data['STANCE1'])
+            # UFC_data = UFC_data.drop(['Unnamed: 0'], axis=1)
+            UFC_data = UFC_data.drop(['NAME'], axis=1)
+            UFC_data = UFC_data.drop(['NAME1'], axis=1)
+            UFC_data = UFC_data.drop('WIN', axis=1)
+
+            cols_to_norm = ['HEIGHT', 'STANCE', 'WEIGHT', 'REACH', 'DOB', 'SLpM', 'StrAcc',
+                            'SApM', 'StrDef', 'TDAvg', 'TDAcc', 'TDDef', 'SubAvg',
+                            'HEIGHT1', 'STANCE1', 'WEIGHT1', 'REACH1', 'DOB1',
+                            'SLpM1', 'StrAcc1', 'SApM1', 'StrDef1', 'TDAvg1', 'TDAcc1',
+                            'TDDef1', 'SubAvg1']
+
+            UFC_data['STANCE'].value_counts().plot(kind='bar', color="red")
+            scaler = MinMaxScaler()
+            UFC_data[cols_to_norm] = scaler.fit_transform(UFC_data[cols_to_norm])
+            # print(UFC_data.iloc[-1])
+            toPredict = np.asarray(UFC_data.iloc[-1]).reshape(1, -1)
+
+            # # result = result.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
+            # print("Datasets2", df, flush=True)
+            # # final_features = np.hstack(df)
+            # # final_features = [x for x in final_features]
+            # # print(type(final_features))
+            # # df = pd.DataFrame(final_features)
+            # # print("#result", result)
+            prediction = model.predict_proba(toPredict)
+            output = prediction
+            # print(prediction[0].round(), flush=True)
+            output = (prediction [0]* 100 )
+            print(output, flush=True)
+            return render_template('index.html', prediction_text="Prediction: Chance Fighter A {}%".format(output))
+
+        else:
+            print("Did not get dataset", flush=True)
+
+            # output = (round(1000, 2) * 100 / 1)
+            #
+            # print(output, flush=True)
+
+        return render_template('index.html')
+    else:
+        return render_template('index.html')
+
+
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    '''
+    for direct api call through request
+    :return:
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict_([np.array(list(data.values))])
+    output = prediction[0]
+    return jsonify(output)
+
+    # return jsonify(output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ WebAPP/static/data2.js
@@ -0,0 +1,296 @@
+let $select = $('#Name');
+
+let script = document.currentScript;
+let fullUrl = script.src;
+let jsonUrl = fullUrl.replace("data2.js", "fighterDataset2.json") // hard coded jsonUrl
+
+
+
+
+$.getJSON(jsonUrl, function (datas) {
+    // $select.html('');
+
+    const name =  $('li.name1').text();
+    const height = $('li.height1').text();
+    const weight = $('li.weight1').text();
+    const reach = $('li.reach1').text();
+    const stance = $('li.stance1').text();
+    const dob = $('li.dob1').text();
+    const name1 =  $('li.name2').text();
+    const height1 = $('li.height2').text();
+    const weight1 = $('li.weight2').text();
+    const reach1 = $('li.reach2').text();
+    const stance1 = $('li.stance2').text();
+    const dob1 = $('li.dob2').text();
+
+    // const sLpM = data['SLpM']
+    // const strAcc = data['Str. Acc..']
+    // const sApM = data['SApM']
+    // const strDef = data['Str. Def']
+    // const tdAvg = data['TD Avg']
+    // const tdAcc = data['TD Acc']
+    // const tdDef = data['TD Def.']
+    // const subAvg = data['Sub. Avg']
+
+
+    let data_list =[]
+    let name_set = []
+    let name_set2 = []
+    let fighter_one = []
+    let fighter_two = []
+    let radar_one = []
+    let radar_two = []
+
+
+
+// && name !== null
+//             console.log(name[7+5])
+
+    for (let i = 0; i < 3582; i++) {
+        if (typeof datas[i] !== 'undefined')
+        {
+            data_list.push(datas[i]);
+
+        }
+    };
+
+    console.log("data", data_list[9]);
+    console.log("data", data_list.length );
+
+    for (let i = 0; i < data_list.length; i++) {
+        $('#Name').append('<option id="' + i + '">' + data_list[i].NAME + '</option>');
+        $('#Name2').append('<option id="' + i + '">' + data_list[i].NAME + '</option>');
+        name_set.push(data_list[i].NAME);
+        name_set2.push(data_list[i].NAME);
+
+    }
+
+
+    $(document).ready(function() {
+        let data = name_set
+        let data2 = name_set2
+
+        
+       $('#Name').select2( (data))
+    //    $('#Name2').select2( (data2))
+
+    });
+
+    // fighter_1.select2()
+    // $(document).ready(function() {
+        
+        
+    //     $('#Name2').select2((  data = name_set2 ));
+    // });
+  
+
+
+    //   $('#height1').html( '')
+
+
+      $('#Name').change(function(){
+        let fighter_id = $(this).children(":selected").attr("id");
+
+            $('li.height1').text(height+ data_list[fighter_id].HEIGHT);
+            $('li.weight1').text(weight+ data_list[fighter_id].WEIGHT);
+            $('li.reach1').text(reach+ data_list[fighter_id].REACH);
+            $('li.stance1').text(stance+ data_list[fighter_id].STANCE);
+            $('li.dob1').text(dob+ data_list[fighter_id].DOB);
+            // $('#fighter-attributea').css('width','300px');
+
+ 
+            if ($('#Name').children(":selected").attr("value") !== 'none'
+                && $('#Name2').children(":selected").attr("value") !== 'none') {
+
+            let fighter_id = $('#Name').children(":selected").attr("id");
+            let fighter_id2 = $('#Name2').children(":selected").attr("id");
+            console.log("fighter one :",data_list[fighter_id])
+            console.log("fighter two :",data_list[fighter_id2])
+            fighter_one = []
+            fighter_two = []
+            fighter_one.push(data_list[fighter_id].NAME )
+            fighter_one.push(data_list[fighter_id].HEIGHT )
+            fighter_one.push(data_list[fighter_id].WEIGHT )
+            fighter_one.push(data_list[fighter_id].REACH )
+            fighter_one.push(data_list[fighter_id].STANCE )
+            fighter_one.push(data_list[fighter_id].DOB )
+            fighter_one.push(data_list[fighter_id].SLpM )
+            fighter_one.push(data_list[fighter_id].StrAcc)
+            fighter_one.push(data_list[fighter_id].SApM )
+            fighter_one.push(data_list[fighter_id].StrDef)
+            fighter_one.push(data_list[fighter_id].TDAvg)
+            fighter_one.push(data_list[fighter_id].TDAcc)
+            fighter_one.push(data_list[fighter_id].TDDef)
+            fighter_one.push(data_list[fighter_id].SubAvg)
+            fighter_two.push(data_list[fighter_id2].NAME )
+            fighter_two.push(data_list[fighter_id2].HEIGHT )
+            fighter_two.push(data_list[fighter_id2].WEIGHT )
+            fighter_two.push(data_list[fighter_id2].REACH )
+            fighter_two.push(data_list[fighter_id2].STANCE )
+            fighter_two.push(data_list[fighter_id2].DOB )
+            fighter_two.push(data_list[fighter_id2].SLpM )
+            fighter_two.push(data_list[fighter_id2].StrAcc)
+            fighter_two.push(data_list[fighter_id2].SApM )
+            fighter_two.push(data_list[fighter_id2].StrDef)
+            fighter_two.push(data_list[fighter_id2].TDAvg)
+            fighter_two.push(data_list[fighter_id2].TDAcc)
+            fighter_two.push(data_list[fighter_id2].TDDef)
+            fighter_two.push(data_list[fighter_id2].SubAvg)
+            // $('#data').val('');
+            // $('#submit_value').click()
+            let radar_one = []
+            let radar_two = []
+            
+            
+
+
+        }
+      })
+
+
+
+     $('#Name2').change(function(){
+
+
+        let fighter_id = $(this).children(":selected").attr("id");
+
+            $('li.height2').text(height1+ data_list[fighter_id].HEIGHT);
+            $('li.weight2').text(weight1+ data_list[fighter_id].WEIGHT);
+            $('li.reach2').text(reach1+ data_list[fighter_id].REACH);
+            $('li.stance2').text(stance1+ data_list[fighter_id].STANCE);
+            $('li.dob2').text(dob1+ data_list[fighter_id].DOB);
+
+            if ($('#Name').children(":selected").attr("value") !== 'none'
+                && $('#Name1').children(":selected").attr("value") !== 'none') {
+
+            let fighter_id2 = $('#Name2').children(":selected").attr("id");
+            let fighter_id = $('#Name').children(":selected").attr("id");
+            console.log("fighter one :",data_list[fighter_id])
+            console.log("fighter two:",data_list[fighter_id2])
+            fighter_one = []
+            fighter_two = []
+            fighter_one.push(data_list[fighter_id].NAME )
+            fighter_one.push(data_list[fighter_id].HEIGHT )
+            fighter_one.push(data_list[fighter_id].WEIGHT )
+            fighter_one.push(data_list[fighter_id].REACH )
+            fighter_one.push(data_list[fighter_id].STANCE )
+            fighter_one.push(data_list[fighter_id].DOB )
+            fighter_one.push(data_list[fighter_id].SLpM )
+            fighter_one.push(data_list[fighter_id].StrAcc)
+            fighter_one.push(data_list[fighter_id].SApM )
+            fighter_one.push(data_list[fighter_id].StrDef)
+            fighter_one.push(data_list[fighter_id].TDAvg)
+            fighter_one.push(data_list[fighter_id].TDAcc)
+            fighter_one.push(data_list[fighter_id].TDDef)
+            fighter_one.push(data_list[fighter_id].SubAvg)
+            fighter_two.push(data_list[fighter_id2].NAME )
+            fighter_two.push(data_list[fighter_id2].HEIGHT )
+            fighter_two.push(data_list[fighter_id2].WEIGHT )
+            fighter_two.push(data_list[fighter_id2].REACH )
+            fighter_two.push(data_list[fighter_id2].STANCE )
+            fighter_two.push(data_list[fighter_id2].DOB )
+            fighter_two.push(data_list[fighter_id2].SLpM )
+            fighter_two.push(data_list[fighter_id2].StrAcc)
+            fighter_two.push(data_list[fighter_id2].SApM )
+            fighter_two.push(data_list[fighter_id2].StrDef)
+            fighter_two.push(data_list[fighter_id2].TDAvg)
+            fighter_two.push(data_list[fighter_id2].TDAcc)
+            fighter_two.push(data_list[fighter_id2].TDDef)
+            fighter_two.push(data_list[fighter_id2].SubAvg)
+            // $('#data').val('');
+            // $('#submit_value').click()
+            let radar_one = []
+            let radar_two = []
+            radar_one.push(fighter_one.slice(1,2).concat(fighter_one.slice(3 ,4)).concat(fighter_one.slice(6,13)));
+            radar_two.push(fighter_two.slice(1,2).concat(fighter_two.slice(3 ,4)).concat(fighter_two.slice(6,13)));
+            radar_one[0][0] = radar_one[0][0] * 10
+            radar_two[0][0] = radar_two[0][0] * 10
+            radar_one[0][2] = radar_one[0][2] * 10
+            radar_two[0][2] = radar_two[0][2] * 10
+            radar_one[0][4] = radar_one[0][4] * 10
+            radar_two[0][4] = radar_two[0][4] * 10
+            radar_one[0][6] = radar_one[0][6] * 10
+            radar_two[0][6] = radar_two[0][6] * 10
+
+            // radar_one.push()
+        
+            console.log(radar_one[0])
+            
+            function convertRange( value, min , max) { 
+                
+                for (let i = 0; i < value[0].length; i++) 
+                {
+                    if(max - min === 0) return 1;
+                    value[0][i] = (value[0][i] - min) / (max - min);
+
+                }
+                return value;
+            };
+            // var num = scale(.25, 0, 1, 0, 100);
+
+
+
+            let chart = convertRange(radar_one ,2 ,10)
+            let chart2 = convertRange(radar_two ,2 ,10)
+            console.log(chart ,"kk")
+
+
+
+
+
+            $(document).ready(function() {
+                var ctx = $("#chart-line");
+                var myLineChart = new Chart(ctx, {
+                    type: 'radar',
+                    data: {
+                        labels: ['HEIGHT', 'REACH', 'SLpM', 'StrAcc', 'SApM', 'StrDef', 'TDAvg','TDAcc','TDDef','SubAvg'],
+                        datasets: [{
+                            data: chart[0],
+                            label: "A Team",
+                            borderColor: "#458af7",
+                            backgroundColor: '#e0d316',
+                            fill: true,
+                            fontColor: 'black',
+                            pointHoverBorderColor: '#eb4034',
+                        }, {
+                            data: chart2[0],
+                            label: "B Team",
+                            borderColor: "#f54242",
+                            fill: true,
+                            backgroundColor: '#3cba9f'
+                        }]
+                    },
+                    options: {
+                        title: {
+                            display: true,
+                            text: ''
+                        }
+                    }
+                });
+            });
+
+                }
+            })
+
+   
+        console.log("new_value",$('#data').val())
+
+        $('#submit_value').click(function(){
+      
+            let fight_data = fighter_one.concat(fighter_two);
+            // console.log(fight_data)
+    
+            if ( fight_data.length  >14){
+                $('#data').val(fight_data);
+                $('#form_value').submit();
+            }
+    
+    
+        console.log(jsonUrl);
+    
+    
+    })
+
+    })
+
+   
