079a2bc6d98e411226096d8e4bf5ef819e64034c
roro-commits <olarotimi@protonmail.com>
2021-01-11 19:14:49 +0000

    web app minimum

--- scrapy /randomforest.py
+++ scrapy /randomforest.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import model_selection
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder as encoder
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import classification_report, confusion_matrix
 
 
 
-UFC_data = pd.read_csv('clean_dataset.csv')
+UFC_data = pd.read_csv('scrapy /clean_dataset.csv')
 UFC_data['STANCE'] = encoder().fit_transform(UFC_data['STANCE'])
 UFC_data['STANCE1'] = encoder().fit_transform(UFC_data['STANCE1'])
 # UFC_data = UFC_data.drop(['Unnamed: 0'], axis=1)
@@ -31,7 +31,7 @@ labels = UFC_data['WIN']
 
 X_train, X_test, y_train, y_test = train_test_split(x_data, labels, test_size=0.33, random_state=200)
 
-rfc = RandomForestRegressor(n_estimators=20, random_state=0)
+rfc = RandomForestClassifier(n_estimators=20, random_state=0)
 rfc.fit(X_train, y_train)
 
 # predictions
